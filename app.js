e2fdef91c29e452db9e6c671dce2baf1f80f2388
Rgstar8497 <47080912+Rgstar8497@users.noreply.github.com>
2025-07-05 20:58:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import webRoutes from './routes/web.js';
 // Connect to MongoDB
 const app = express();
 const PORT = process.env.PORT || 3000;
-const DB_URL = process.env.MONGO_URI || 'mongodb://localhost:27017/school';
+const DB_URL = 'mongodb://0.0.0.0:27017/school';
 connectDB(DB_URL);
 app.use('/student',express.static(join(process.cwd(),'public')));
 app.use('/student/edit',express.static(join(process.cwd(),'public')));
