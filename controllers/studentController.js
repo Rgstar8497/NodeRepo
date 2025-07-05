7106e425e56acd20adbad26636a4bf15c26f8979
Rgstar8497 <47080912+Rgstar8497@users.noreply.github.com>
2025-07-06 00:11:22 +0530

    Add files via upload

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -17,6 +17,23 @@ class StudentController {
             console.error("Error reading data:", error);
         }
     }
+    static createDoc1 = async (req, res) => {
+        try {
+            const result=new Student(req.body);
+            const savedData = await result.save();
+            res.send(savedData);
+        } catch (error) {
+            console.error("Error saving data:", error);
+        }
+    }
+    static getAllDocs1 = async (req, res) => {
+        try {
+            const students = await Student.find({});
+            res.send(students);
+        } catch (error) {
+            console.error("Error reading data:", error);
+        }
+    }
     static editDocs = async (req, res) => {
         try {
             const result = await Student.findById(req.params.id);
@@ -37,6 +54,22 @@ class StudentController {
         try {
             const result= await Student.findByIdAndDelete(req.params.id);
             res.redirect("/student");
+        } catch (error) {
+            console.error("Error updating data:", error);
+        }
+    }
+    static updateDocsById1 = async (req, res) => {
+        try {
+            const result = await Student.findByIdAndUpdate(req.params.id,req.body);
+            res.send(result);
+        } catch (error) {
+            console.error("Error updating data:", error);
+        }
+    }
+    static deleteDocsById1 = async (req, res) => {
+        try {
+            const result= await Student.findByIdAndDelete(req.params.id);
+            res.send(result);
         } catch (error) {
             console.error("Error updating data:", error);
         }
